ba5c109c633ee5989e478d93b57307c47b31ca79
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-02-11 19:35:53 +0100

    Update 3_data_structures_iterators.rb

--- 1-ruby/3_data_structures_iterators.rb
+++ 1-ruby/3_data_structures_iterators.rb
@@ -2,7 +2,8 @@
 #0. Quelle est la différence entre un hash et un array ? 
 =begin
 
-
+Un array indexe une valeur à un index, soit toujours un integer. 
+Pour associer une string, en tant que clé, à une valeur, on utilisera plutôt un hash.
 
 =end
 
@@ -10,9 +11,19 @@
 #1. Définissez et malmenez un Array
 # (y ajouter des éléments, accéder à ses éléments, supprimer des éléments, renvoyer sa taille)
 begin
+voyels = %w(a e i o u)
 
+#voir un élément, le e
+puts voyels[1]
 
+#ajouter un y
+voyels << "y"
 
+#supprimer le o
+voyels.delete_at(3)
+
+#taille de l'array
+voyels.length
 
 end
 
@@ -20,7 +31,23 @@ end
 # (y ajouter des éléments, accéder à ses éléments, supprimer des éléments, renvoyer sa taille)
 begin
 
+fantomette = {
+  name: "Fantomette",
+  age: "10",
+  job: "super spy"
+  }
 
+#voir son nom
+puts fantomette.name
+
+#ajouter son adresse
+fantomette[:address]="private"
+
+#supprimer le o
+fantomette.delete(:address)
+
+#taille de l'array
+fantomette.length
 
 
 end
